bd43c2653593f17b17d787da8ef99bc63012567e
Dan Cheung <dan@snapup.com>
2015-08-06 19:16:31 -0700

    add shipped orders

--- app/overrides/orders_index.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
-  :name => 'add_shippo_button_to_orders_page',
-  :insert_after => "erb[silent]:contains('content_for :page_actions')",
-  :text => "
-      <%= button_link_to 'Shipped Orders', admin_shippo_ship_path, :icon => '', :target => '_blank', :class => 'btn btn-success' %>
-  ")
