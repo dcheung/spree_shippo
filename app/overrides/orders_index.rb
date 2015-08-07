b047d03f4383ca444333a23bbe6b46adbabef4f0
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-06 18:53:21 -0700

    Update orders_index.rb

--- app/overrides/orders_index.rb
+++ app/overrides/orders_index.rb
@@ -2,7 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
   :name => 'add_shippo_button_to_orders_page',
   :insert_after => "erb[silent]:contains('content_for :page_actions')",
   :text => "
-    <li>
-      <%= button_link_to Spree.t(:view_on_shippo), admin_shippo_orders_path, :icon => '', :target => '_blank' %>
-    </li>
+      <%= button_link_to 'Shipping Label', admin_shippo_orders_path, :icon => '', :target => '_blank', :class => 'btn btn-success' %>
   ")
