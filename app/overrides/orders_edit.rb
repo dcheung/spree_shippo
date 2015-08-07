d388821cc7f3ce101bd27590ff440d572845958f
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-06 19:08:17 -0700

    Update orders_edit.rb

--- app/overrides/orders_edit.rb
+++ app/overrides/orders_edit.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/admin/orders/edit',
   :name => 'add_shippo_button_to_order_page',
   :insert_before => "erb[loud]:contains('event_links')",
   :text => '
-      <%= button_link_to "Shipping Label", admin_shippo_order_path(@order.id), :icon => "", :target => "_blank", :class => "btn btn-success" %>
+      <%= button_link_to "Print Shipping Label", admin_shippo_order_path(@order.id), :icon => "", :target => "_blank", :class => "btn btn-success" %>
   ')
